81f0ecda1d75b0b2fdd8fa7d654f0e8f806e8070
Fei Qin <68352557+FeiQin92@users.noreply.github.com>
2022-05-09 15:51:01 -0400

    Add files via upload

--- src/flowCalcCpp.cpp
+++ src/flowCalcCpp.cpp
@@ -1,15 +1,14 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 #include <RcppArmadillo.h>
 
-using namespace Rcpp;
 
 //' Matrix calculation in RcppArmadillo.
 //'
 //' @param Am matrix
 //' @param Cm matrix
-//' @return Matrix calculation, that is \code{inv(Am)%*%Cm}
-// [[Rcpp::export(flowCalcCpp)]]
-arma::mat flowCalcCpp(arma::mat Am, arma::mat Cm) 
+//' @return Matrix, that is \code{inv(Am)%*%Cm}
+// [[Rcpp::export]]
+arma::mat flowCalcCpp(const arma::mat Am, const arma::mat Cm) 
 {
          arma::mat B = inv(Am) * Cm;
          return B;
